1b01beab446ba471c44bc6cd45df10a8042232cb
Giovanni <giolucarna@gmail.com>
2022-04-07 14:22:11 -0300

    Feat :tada: - Added new validations and 2 new classes

---
+++ TP_1/Lucchetta.Giovanni.2A.TP1/Entidades/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static char ValidarOperador (char operando)
+        {
+            char validar = '+';
+
+            if(operando != '-' || operando != '/' || operando != '*')
+            {
+                validar = operando;
+            }
+
+            return validar;
+        }
+
+        public static double Operar(Operando numero, Operando numero1, char operador)
+        {
+
+        }
+    }
+}
---
+++ TP_1/Lucchetta.Giovanni.2A.TP1/Entidades/Operando.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Operando
+    {
+        private double numero;
+
+        private string Numero
+        {
+            set
+            {
+               this.numero = ValidarOperando(value);
+            }
+        }
+
+        public Operando()
+        {
+            numero = 0;
+        }
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+
+        private double ValidarOperando(string strNumero)
+        {
+            double strValidado = 0;
+
+            double.TryParse(strNumero, out strValidado);
+
+            return strValidado;
+        }
+
+        private bool EsBinario(string binario)
+        {
+            bool binaryValidation = true;
+
+            for(int i = 0; i < binario.Length; i++)
+            {
+                if (binario[i] != 0 || binario[i] != 1)
+                {
+                    binaryValidation = false;
+                    break;
+                }
+            }
+           
+            return binaryValidation;
+        }
+
+
+    }
+}
